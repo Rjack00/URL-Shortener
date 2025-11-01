be2ab4e9e6f22e884926b1b8b97ca2e3da4a443b
Rjack00 <Rjackson77@comcast.net>
2025-10-31 18:08:25 -0600

    index.js updated

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//enable body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// URL shortner \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 // Middleware to see (help visualize as I learn) request object items for GET and POST
 app.use((req, res, next) => { 
@@ -34,25 +36,74 @@ app.use((req, res, next) => {
   next();
 });
 
+// URL shortner \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 //Helper function to validate URL format
 function isValidUrl (urlString) {
   try {
     const url = new URL(urlString);
+    //must be http or https protocol
     return url.protocol === 'http:' || url.protocol === 'https:';
   } catch (err) {
     return false;
   }
 }
 
+const urlDatabase = {};
+let urlCounter = 1;
 
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
 
-const urlDataBase = {};
-let urlCounter = 0;
+  // validate the URL
+  if (!isValidUrl (originalUrl)) {
+    return res.json({error: 'invalid url'});
+  }
 
-app.get('/api/shorturl', (req, res) => {
+  // check if URL already exists in database
+  const existingUrl = Object.keys(urlDatabase).find(
+    key => urlDatabase[key] === originalUrl
+  );
+ 
+  if (existingUrl) {
+    return res.json({
+      original_url : originalUrl,
+      short_url : parseInt(existingUrl)
+    });
+  } 
+
+  //create new short URL
+  const shortUrl = urlCounter;
+  urlDatabase[shortUrl] = originalUrl;
+  urlCounter++;
+
+  res.json({
+    original_url : originalUrl,
+    short_url : shortUrl
+  })
   
-})
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  console.log('shortUrl: ', shortUrl);
+  console.log('urlDatabase: ', shortUrl);
+
+  const originalUrl= urlDatabase[shortUrl];
+  console.log('Found URL:', originalUrl);
+
+  if (!originalUrl) {
+    return res.json({
+      error: 'No short URL found'
+    });
+   }
+    res.redirect(originalUrl);
+ 
+});
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
